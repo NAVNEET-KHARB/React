6b8d46fb43eb77759e5ec7a23a085a5b338c2096
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-02-02 14:43:05 +0530

    Added Update functionality

--- 11redux/src/features/todo/todoSlice.js
+++ 11redux/src/features/todo/todoSlice.js
@@ -18,9 +18,15 @@ export const todoSlice = createSlice({
     removeTodo: (state, action) => {
       state.todos = state.todos.filter((todo) => todo.id !== action.payload);
     },
+    updateTodo: (state, action) => {
+      const { id, text } = action.payload;
+      state.todos = state.todos.map((todo) =>
+        todo.id === id ? { ...todo, text: text } : todo
+      );
+    },
   },
 });
 
-export const { addTodo, removeTodo } = todoSlice.actions;
+export const { addTodo, removeTodo, updateTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
