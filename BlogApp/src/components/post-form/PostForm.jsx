bb06648db519f01c09f613b5bb8018a1e8f0c651
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-02-18 22:45:44 +0530

    Completed PostForm

--- BlogApp/src/components/post-form/PostForm.jsx
+++ BlogApp/src/components/post-form/PostForm.jsx
@@ -1,21 +1,130 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import Button from "../Button";
 import Input from "../Input";
 import RTE from "../RTE";
 import Select from "../Select";
 import appwriteService from "../../appwrite/config";
-import { UseSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-function PostForm({post}) {
-    const {} = useForm({
-        title : post?.title||""
-    })
+function PostForm({ post }) {
+  const { register, handleSubmit, setValue, watch, control, getValues } =
+    useForm({
+      defaultValues: {
+        title: post?.title || "",
+        slug: post?.slug || "",
+        content: post?.content || "",
+        status: post?.status || "active",
+      },
+    });
+  const navigate = useNavigate();
+  const userData = useSelector((state) => state.auth.userData);
+  const submit = async (data) => {
+    if (post) {
+      const file = data.image[0]
+        ? await appwriteService.uploadFile(data.image[0])
+        : null;
+      if (file) {
+        await appwriteService.deleteFile(post.featuredImage);
+      }
+      const updPost = await appwriteService.updatePost(post.$id, {
+        ...data,
+        featuredImage: file ? file.$id : undefined,
+      });
+      if (updPost) {
+        navigate(`/post/${updPost.$id}`);
+      }
+    } else {
+      const file = await appwriteService.uploadFile(data.image[0]);
+      if (file) {
+        const fileId = file.$id;
+        data.featuredImage = fileId;
+        const nPost = appwriteService.createPost({
+          ...data,
+          userId: userData.$id,
+        });
+        if (nPost) {
+          navigate(`/post/${nPost.$id}`);
+        }
+      }
+    }
+  };
+  const slugTransform = useCallback((value) => {
+    if (value && typeof value === "string") {
+      return value
+        .trim()
+        .toLowerCase()
+        .replace(/[^a-zA-Z\d\s]+/g, "-")
+        .replace(/\s/g, "-");
+    }
+  }, []);
+  useEffect(() => {
+    watch((value, { name }) => {
+      if (name === "title") {
+        setValue("slug", slugTransform(value.title), { shouldValidate: true });
+      }
+    });
+  }, [watch, setValue, slugTransform]);
   return (
-    <div>
-      
-    </div>
-  )
+    <form onSubmit={handleSubmit(submit)} className="flex flex-wrap">
+      <div className="w-2/3 px-2">
+        <Input
+          label="Title :"
+          placeholder="Title"
+          className="mb-4"
+          {...register("title", { required: true })}
+        />
+        <Input
+          label="Slug :"
+          placeholder="Slug"
+          className="mb-4"
+          {...register("slug", { required: true })}
+          onInput={(e) => {
+            setValue("slug", slugTransform(e.target.value), {
+              shouldValidate: true,
+            });
+          }}
+        />
+        <RTE
+          label="Content :"
+          control={control}
+          name="content"
+          defaultValues={getValues("content")}
+        />
+      </div>
+      <div className="w-1/3 px-2">
+        <Input
+          label="Featured Image"
+          type="file"
+          className="mb-4"
+          accept="image/png,image/jpg,image/jpeg"
+          {...register("image", { required: !post })}
+        />
+        {post && (
+          <div className="w-full mb-4">
+            <img
+              src={appwriteService.getPreview(post.featuredImage)}
+              alt={post.title}
+              className="rounded-lg"
+            />
+          </div>
+        )}
+        <Select
+          options={["active", "inactive"]}
+          label="Status"
+          className="mb-4"
+          {...register("status", { required: true })}
+        />
+        <Button
+          type="submit"
+          bgColor={post ? "bg-green-600" : undefined}
+          className="w-full"
+        >
+          {post ? "Update" : "Submit"}
+        </Button>
+      </div>
+    </form>
+  );
 }
 
-export default PostForm
+export default PostForm;
